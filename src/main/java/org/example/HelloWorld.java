a1f26ef83cb663df870242293a03ae6cf3a5adfe
Joel Medina <92224207+RoJoMedina@users.noreply.github.com>
2025-01-16 04:17:00 -0600

    Cambio de mensaje a Hello, Collaboration!

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello, New Branch!");
+        System.out.println("Hello, Collaboration!");
     }
 
 
